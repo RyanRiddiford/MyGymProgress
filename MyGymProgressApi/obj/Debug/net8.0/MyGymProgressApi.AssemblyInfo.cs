fbb6949dd50332c6f596cd13f72986171124251f
Ryan Riddiford <menialcub211@gmail.com>
2024-05-25 18:47:28 +1000

    Updated path to dockerfile in buildspec

--- MyGymProgressApi/obj/Debug/net8.0/MyGymProgressApi.AssemblyInfo.cs
+++ MyGymProgressApi/obj/Debug/net8.0/MyGymProgressApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyGymProgressApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59b50ca155425fda0817dc44c230955fc9951851")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d54923b6c444ec679a6a45d6b40b06da90109f64")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyGymProgressApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyGymProgressApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
